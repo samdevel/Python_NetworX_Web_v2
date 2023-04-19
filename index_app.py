4d4535ddf85e2af46e2474c0d2890c604345d692
sm@Devel <sam_manay@yahoo.com>
2023-04-20 00:34:40 +0800

    Update index_app.py

--- index_app.py
+++ index_app.py
@@ -14,7 +14,7 @@ from credentials import switch_credentials
 # This software released mainly for education and You may distribute and                        #
 # you may develop it for your own purpose                                                       #
 # Activities other than copying, distribution and modification are not covered by this License  #
-# You may accept at your own risks for using this app                                           #
+# You may accept at your own risks of using this app                                           #
 # Good day and happy coding                                                                     #
 #################################################################################################
 
